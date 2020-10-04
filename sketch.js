48ed9f68fe5c39c332e3672e34de8f673ab9f6ba
Tiago Duarte Ricardo <tribaltronik@gmail.com>
2020-10-04 10:49:45 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -88,10 +88,13 @@ function RegistaVisualizacao(familiaID){
     if (familiaID != undefined && familiaID > 0) {
     tracking.push(familiaID);
     }
-    var tipo = ObtemTipoCliente(tracking);
-    atualizaRecomendacoes(tipo);
-    localStorage.setItem('tipo',tipo );
-    console.log('tipo: ' + tipo);
+    if(tracking.length > 2)
+    {
+        var tipo = ObtemTipoCliente(tracking);
+        atualizaRecomendacoes(tipo);
+        localStorage.setItem('tipo',tipo );
+        console.log('tipo: ' + tipo);
+    }
     var jsonString = JSON.stringify(tracking);
     localStorage.setItem('tracking',jsonString );
 }
